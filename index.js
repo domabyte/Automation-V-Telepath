afdbb6ea2aec6604af525a1da332be45731d2fef
Dikshit <singhdikshit69@gmail.com>
2023-02-23 21:56:10 +0530

    initiate bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const {Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx)=>ctx.reply('Welcome'));
+
+bot.on('sticker',(ctx)=>ctx.reply('👍'));
+bot.hears('hi',(ctx)=>ctx.reply("Hey there!!"));
+
+bot.launch();
