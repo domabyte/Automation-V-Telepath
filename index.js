5f2718ec8e53877bd5879c6f25100966d3d36ffd
Dikshit <singhdikshit69@gmail.com>
2023-02-24 13:14:41 +0530

    integrate open-ai version file into main file

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const {Telegraf } = require("telegraf");
 const dotenv = require("dotenv");
+const tele = require("./open-ai/version1");
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx)=>ctx.reply('Welcome'));
 
-bot.on('sticker',(ctx)=>ctx.reply('👍'));
-bot.hears('hi',(ctx)=>ctx.reply("Hey there!!"));
+bot.start((ctx)=>ctx.reply('Welcome'));
+bot.on('message',(ctx)=>{
+    let texture = ctx.message.text;
+    let splitTexture = texture.split(": ");
+    let textCode = splitTexture[0];
+    let query = splitTexture[1];
+
+    switch(textCode){
+        case 'Q':
+            case 'q':
+                tele.teleV(query).then((data)=>{
+                    ctx.reply(data);
+                }).catch(err=>console.log(err));
+                break;
+
+        default:
+            ctx.reply("Just ask question. Don't spam here!!");
+            break;
+    }
+})
 
 bot.launch();
