e9b7cfa69f38cc9789daa1e222c483429fe47660
Dikshit <singhdikshit69@gmail.com>
2023-02-26 15:15:45 +0530

    Add testing folder for test codes

---
+++ Testing_codes/test_SD.js
@@ -0,0 +1,9 @@
+const {spawn} = require("child_process");
+
+const query = "A digital illustration of a steampunk library with clockwork machines, 4k, detailed, trending in artstation, fantasy vivid colors";
+
+const response = spawn('python',['../py_modules/stable_diffusion.py',query]);
+
+response.stdout.on('data',(data)=>{
+    console.log(data.toString());
+})
