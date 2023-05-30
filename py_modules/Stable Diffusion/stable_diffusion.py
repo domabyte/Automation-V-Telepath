ae433f1791561cead03859bfc7e9a7e2d5c1a48e
Dikshit <singhdikshit69@gmail.com>
2023-05-30 14:14:59 +0530

    Remove older version & remove MODAL_VERSION

--- py_modules/Stable Diffusion/stable_diffusion.py
+++ py_modules/Stable Diffusion/stable_diffusion.py
@@ -1,35 +1,14 @@
-#imports
 from cmd import PROMPT
-import sys
 import replicate
-from dotenv import load_dotenv
 import os
-
-#loading the dotenv file
+import sys
+from dotenv import load_dotenv
 load_dotenv()
+REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
 
-#command line argument
 PROMPT = sys.argv[1]
 
-#Replicate api
-REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
-MODAL_VERSION =  os.getenv('MODAL_VERSION')
-
-
-#model
-# client = replicate.Client(api_token=REPLICATE_API_TOKEN)
-# print("client",client)
-# version = client.versions.get("stability-ai/stable-diffusion")
-# print(version)
-# image_urls = version.predict(prompt=PROMPT)
-# print(image_urls)
-
-model = replicate.models.get("stability-ai/stable-diffusion")
-
-version = model.versions.get(MODAL_VERSION)
-
-image_urls = version.predict(prompt=PROMPT)
-print(image_urls);
-
-
-
+client = replicate.Client(api_token=REPLICATE_API_TOKEN)
+model = client.models.get("stability-ai/stable-diffusion")
+image_urls = model.predict(prompt=PROMPT)
+print(image_urls)
